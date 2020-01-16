17747694503b5d01b38c0b99f94499aa100f55c9
Kristina Ilyovska <26176057+lyloster@users.noreply.github.com>
2020-01-16 15:35:18 -0500

    Convert input to int, general print message

--- PythonII-ControlStructures/Exercises/getInt.py
+++ PythonII-ControlStructures/Exercises/getInt.py
@@ -1,12 +1,16 @@
+#This exercise is identical to the previous exercise,
+#except that instead of printing "Cannot convert!" when my_var
+#cannot be converted to an integer, you should instead return
+#the error message that results. For a reminder of how to
+#access the error message in the except block, check out
+#3.5.3, specifically CatchingASpecificError-4.py from 3.5.3.3
+#and CatchingMultipleSpecificErrors-5.py from 3.5.3.4.
+#
 #Write a function called get_integer that takes as input one
 #variable, my_var. If my_var can be converted to an integer,
 #do so and return that integer. If my_var cannot be converted
-#to an integer, return a message that says, "Cannot convert!"
-#
-#For example, for "5" as the value of my_var, get_integer would
-#return the integer 5. If the value of my_var is the string
-#"Boggle.", then get_integer would return a string with the
-#value "Cannot convert!"
+#to an integer, return the error message that results from
+#attempting to do so.
 #
 #Do not use any conditionals or the type() function.
 
@@ -15,17 +19,16 @@
 def get_integer(my_var):
     try:
         return int(my_var)
-    except:
-        return "Cannot convert!"
+    except ValueError as error:
+        return error
 
 
-#Below are some lines of code that will test your function.
-#You can change the value of the variable(s) to test your
-#function with different inputs.
-#
-#If your function works correctly, this will originally
-#print: 5, Cannot convert!, and 5.
-
+#You can use the lines below to test out your function. When
+#the function is written correctly, the output of these three
+#lines should be:
+#5
+#invalid literal for int() with base 10: 'Boggle.'
+#5
 print(get_integer("5"))
 print(get_integer("Boggle."))
 print(get_integer(5.1))
